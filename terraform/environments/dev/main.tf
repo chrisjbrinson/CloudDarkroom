9c2edb25d6c907bd3abf9f0fbcb3c22f3ad7359b
chrisjbrinson <chrisjbrinson@gmail.com>
2026-06-22 18:52:50 -0600

    Configure remote Terraform state

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.10"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "clouddarkroom-brinson-tfstate"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
