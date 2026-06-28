2ae050a03ea0b14b9cfff8a98e067f0e413a3d94
chrisjbrinson <chrisjbrinson@gmail.com>
2026-06-27 18:27:47 -0600

    adding a common db connection script

---
+++ common/db.py
@@ -0,0 +1,12 @@
+import os
+
+import psycopg
+
+
+def get_connection():
+    return psycopg.connect(
+        host=os.environ["DB_HOST"],
+        dbname=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+    )
