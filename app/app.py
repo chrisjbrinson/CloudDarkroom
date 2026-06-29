7706357cc0d1798512560c764bea89c52d120f01
chrisjbrinson <chrisjbrinson@gmail.com>
2026-06-28 18:46:40 -0600

    adding /images endpoint to the app

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import logging
 import os
 import boto3
 import uuid
+from common.db import get_connection
 
 logging.basicConfig(level=logging.INFO)
 
@@ -44,6 +45,50 @@ def upload():
     app.logger.info(f"Finished upload: {file.filename}")
     return f"Uploaded {key} to {bucket}"
 
+
+
+
+@app.get("/images")
+def get_images():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                SELECT
+                    id,
+                    original_filename,
+                    original_bucket,
+                    original_key,
+                    processed_bucket,
+                    processed_key,
+                    status,
+                    width,
+                    height,
+                    created_at,
+                    processed_at
+                FROM images
+                ORDER BY id DESC;
+            """)
+
+            rows = cur.fetchall()
+
+    images = []
+
+    for row in rows:
+        images.append({
+            "id": row[0],
+            "original_filename": row[1],
+            "original_bucket": row[2],
+            "original_key": row[3],
+            "processed_bucket": row[4],
+            "processed_key": row[5],
+            "status": row[6],
+            "width": row[7],
+            "height": row[8],
+            "created_at": row[9].isoformat(),
+            "processed_at": row[10].isoformat() if row[10] else None
+        })
+
+    return images
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
